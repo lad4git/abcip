832b1e1ff78413818bb8236c1e8c80a950183927
shaslad <shaslad@cisco.com>
2025-09-17 16:17:29 -0400

    feat: Add version 2.0.0 header with enhanced packet crafting capabilities

--- src/app/main.cc
+++ src/app/main.cc
@@ -7,6 +7,7 @@
 // Software Foundation, either version 3 of the License, or (at your
 // option) any later version.
 //
+// Version: 2.0.0 - Enhanced packet crafting capabilities
 // This program is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
